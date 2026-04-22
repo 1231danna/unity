78761bf5c1b6167d6e7aa627674076454452242c
yaoc1224 <yaoc1224@gmail.com>
2026-04-22 21:27:09 +0800

    UI

--- Assets/Scripts/NewspaperInteract.cs
+++ Assets/Scripts/NewspaperInteract.cs
@@ -2,14 +2,18 @@ using UnityEngine;
 
 public class NewspaperInteract : MonoBehaviour
 {
-    public GameObject newspaperUIPanel; // 拖入层级中的 NewspaperUI Panel
+    // 将类型从 GameObject 改为 NewspaperManager，这样可以直接调用它的方法
+    public NewspaperManager newspaperManager;
 
     void OnMouseDown()
     {
-        // 显示 UI
-        newspaperUIPanel.SetActive(true);
+        if (newspaperManager != null)
+        {
+            // 关键：调用这个方法，它内部已经包含了面板显示和相机锁定的逻辑
+            newspaperManager.OnOpenNewspaper();
+        }
 
-        // 释放鼠标：如果你的游戏是第一人称控制，需解锁鼠标
+        // 释放鼠标
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
